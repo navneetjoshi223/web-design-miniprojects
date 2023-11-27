91bc9f28edc6a19f870264c481259d9346781162
navneetjoshi223 <joshi.nav@northeastern.edu>
2023-11-26 23:07:33 -0500

    Hourly forecast

--- assignment 10/weatherly/src/components/hourlyforecast/HourlyForecast.js
+++ assignment 10/weatherly/src/components/hourlyforecast/HourlyForecast.js
@@ -1,17 +1,57 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const HourlyForecast = (props) => {
   const { day } = useParams();
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     console.log(props.forecastData);
-  }, [props.forecastData]);
+    const dayNames = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let filteredForecastData = [];
+    props.forecastData.forEach((data) => {
+      let dayNumber = new Date(data.dt_txt).getDay();
+      if (dayNames[dayNumber] === day) {
+        filteredForecastData.push(data);
+      }
+    });
+    console.log("Filtered data for this day", filteredForecastData);
+    setFilteredData(filteredForecastData);
+  }, [props.forecastData, day]);
 
   return (
-    <div>
+    <div className="row">
       <h2>Hourly forecast for {day}</h2>
-      {/* Display hourly forecast information */}
+
+      {filteredData &&
+        filteredData.map((forecast, index) => (
+          <div
+            key={index}
+            className="card border-primary m-3 hourly-weather-card"
+            style={{ width: "18rem" }}
+          >
+            <div className="card-body">
+              <h5 className="card-title">{forecast.dt_txt}</h5>
+              <img
+                src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@4x.png`}
+                alt="Weather icon"
+              />
+              <h3 className="card-text">
+                <b>{forecast.main.temp} &deg;C</b>
+              </h3>
+              <p className="card-text">Min.: {forecast.main.temp_min} &deg;C</p>
+              <p className="card-text">Max.: {forecast.main.temp_max} &deg;C</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
